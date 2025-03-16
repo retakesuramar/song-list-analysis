8c565281acc4803e017bdecad2f5d0f67895769e
retakesuramar <aj.burley@gmail.com>
2025-03-16 22:51:13 +0000

    Refactor

--- SongListAnalysis.kt
+++ SongListAnalysis.kt
@@ -3,30 +3,29 @@
 fun main() {
     val whichOptionToRun = 1
     when(whichOptionToRun) {
-        1 -> doesEveryLineContainExactlyOneCarat()
+        1 -> simpleCheck(::containsExactlyOneCaratIfNecessary)
         else -> println("Unknown option")
     }
     println("Program ended")
 }
 
-private fun doesEveryLineContainExactlyOneCarat() {
+private fun simpleCheck(predicate: (String, Boolean) -> Boolean) {
     var result = true
     for (line in TO_INSTALL.split("\n")) {
-        if (line.isBlank()) continue
-        if (line.startsWith("*(")) continue
-        if (line.count{it == '^'} != 1) {
+        if (!predicate(line, true)) {
             println(line)
             result = false
         }
     }
     for (line in NOT_TO_INSTALL.split("\n")) {
-        if (line.isBlank()) continue
-        if (line.startsWith("*(")) continue
-        if (line.count{it == '^'} != 1) {
+        if (!predicate(line, false)) {
             println(line)
             result = false
         }
     }
     println("result: $result")
 }
- 
+
+private fun containsExactlyOneCaratIfNecessary(line: String, ignored: Boolean) = line.isBlank()
+    || line.startsWith("*(")
+    || (line.count{it == '^'} == 1)
