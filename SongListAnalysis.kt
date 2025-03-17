f7a298bb53a7212e07c201b2659b1bc90ab993e9
retakesuramar <aj.burley@gmail.com>
2025-03-17 00:25:22 +0000

    Add origin validation

--- SongListAnalysis.kt
+++ SongListAnalysis.kt
@@ -5,6 +5,7 @@ fun main() {
     when(whichOptionToRun) {
         1 -> simpleCheck(::containsExactlyOneCaratIfNecessary)
         2 -> simpleCheck(::footnoteValid)
+        3 -> simpleCheck(::originsValid)
         else -> println("Unknown option")
     }
     println("Program ended")
@@ -46,4 +47,28 @@ private fun footnoteValid(line: String, toInstall: Boolean): Boolean {
         return footnoteIndex <= 7
     else
        return footnoteIndex <= 19
+}
+
+private fun originsValid(line: String, toInstall: Boolean): Boolean {
+    if (line.isBlank()) return true
+    if (line.startsWith("*(")) return true
+    val withoutFootnote = line.substringBeforeLast(" ")
+    val origins = "(" + withoutFootnote.substringAfterLast("(")
+    
+    if (!origins.endsWith(")")) return false
+    if (origins.equals("(original)", ignoreCase = true)) return false
+    
+    val parts = origins.split(" ")
+    if (parts.any{it.endsWith("-U")}) return false
+    if (parts.any{it.endsWith("-SP")}) return false
+    if (parts.any{it.endsWith("-Korea")}) return false
+    
+    if (parts.size % 2 == 0) return false
+    val numOrigins = (parts.size + 1) / 2
+    for (i in 1..(numOrigins-1)) {
+        val slashPos = (i-1)*2 + 1
+        if (parts[slashPos] != "/") return false
+    }
+    
+    return true
 }
